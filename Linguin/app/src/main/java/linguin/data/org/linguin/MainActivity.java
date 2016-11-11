d13d1939cb48bfbf6afdec7fc213f9f4d939b7bd
Jordan Tang <jordantang0229@gmail.com>
2016-11-10 18:03:28 -0800

    added user login, stores email in shared preferences.  Added a setting for a 3 column grid view adjustable in settings, saved in shared preferences

--- Linguin/app/src/main/java/linguin/data/org/linguin/MainActivity.java
+++ Linguin/app/src/main/java/linguin/data/org/linguin/MainActivity.java
@@ -1,10 +1,17 @@
 package linguin.data.org.linguin;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -21,7 +28,8 @@ import linguin.data.org.linguin.sample.SampleDataProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressWarnings("FieldCanBeLocal")
+    private static final int SIGNIN_REQUEST = 1001;
+    public static final String MY_GLOBAL_PREFS = "my global prefs";
     private List<DataItem> dataItemList = SampleDataProvider.dataItemList;
     private List<String> itemNames = new ArrayList<String>();
     /**
@@ -43,46 +51,52 @@ public class MainActivity extends AppCompatActivity {
         });
 
         DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean grid = settings.getBoolean(getString(R.string.pref_display_grid), false);
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvItems);
+        if(grid) {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        }
         recyclerView.setAdapter(adapter);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_signin:
+                Intent intent = new Intent(this, SigninActivity.class);
+                startActivityForResult(intent, SIGNIN_REQUEST);
+                return true;
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, PrefsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
+        if (resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST) {
+            String email = data.getStringExtra(SigninActivity.EMAIL_KEY);
+            Toast.makeText(this, "You signed in as " + email, Toast.LENGTH_SHORT).show();
+
+            SharedPreferences.Editor editor = getSharedPreferences(MY_GLOBAL_PREFS, MODE_PRIVATE).edit();
+            editor.putString(SigninActivity.EMAIL_KEY, email);
+            editor.apply();
+
+        }
+
     }
 }
